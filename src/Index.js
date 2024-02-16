cbac42123db8f37a2f1c4b83cb0c86a1b4c4dd20
Muhoza John <muhozajohn250@gmail.com>
2024-02-17 01:31:23 +0200

    adding helper

---
+++ src/Index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import router from "./routes/index.js";
+import dbConnector from "./App.js";
+import morgan from "morgan";
+import cors from "cors";
+import session from "express-session";
+const app = express();
+dotenv.config();
+
+// Configurations
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "anything",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000, // Session expiration time in milliseconds (1 hour)
+      httpOnly: true,
+      secure: false, // Set to true in production with HTTPS
+    },
+  })
+);
+
+app.use(express.json());
+// Routes
+app.use("/api/", router);
+dbConnector;
+const PORT = process.env.PORT || 4200;
+
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Server running on port: http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    author: "theyCallMeDeSaint",
+    message: "Welcome to the IssaPortfolio API!",
+  });
+});
